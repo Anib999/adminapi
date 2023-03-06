ebe0f2c813b958d81ecb58cc0ea87bc2f60a0046
Anib999 <saliv.mah@gmail.com>
2023-03-06 21:30:19 +0545

    alembic

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-DATABASE_URL = f'psycopg2://${settings.database_username}:{settings.database_password}@{settings.database_hostname}/${settings.database_name}'
+DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
 
 engine = create_engine(DATABASE_URL)
 
--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from . import models
 from .routers import client, general, management, sales
+from .database import engine
 
-app = FastAPI(swagger_ui_parameters={'defaultModelswExpandDepth': -1})
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(swagger_ui_parameters={'defaultModelsExpandDepth': -1})
 
 app.include_router(client.router)
 app.include_router(general.router)
